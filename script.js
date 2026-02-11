9f3f15a0be8fc390181f95aed6c944d94ae5170b
Camilo-Becerra <camilo.becerra-figueredo@mohawkcollege.ca>
2026-02-11 17:03:40 -0500

    change js to handle libraries from an array

--- script.js
+++ script.js
@@ -1,136 +1,42 @@
+let markers = [];
+
 function initMap() {
     const bogota = { lat: 4.6097, lng: -74.0817 };
     
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 11,
+        zoom: 12,
         center: bogota
     });
-    const library1 = { lat: 4.6392, lng: -74.1003 };
-    const marker1 = new google.maps.Marker({
-        position: library1,
-        map: map,
-        title: "Biblioteca Virgilio Barco"
-    });
-    const infoWindow1 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca Virgilio Barco</h5><p>Av. Carrera 60 #57-60</p>"
-    });
     
-    marker1.addListener("click", function() {
-        infoWindow1.open(map, marker1);
-    });
-    const library2 = { lat: 4.5981, lng: -74.0758 };
-    const marker2 = new google.maps.Marker({
-        position: library2,
-        map: map,
-        title: "Biblioteca El Tintal"
-    });
-    const infoWindow2 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca El Tintal</h5><p>Av. Ciudad de Cali #6C-09</p>"
-    });
-    marker2.addListener("click", function() {
-        infoWindow2.open(map, marker2);
-    });
+    const libraries = [
+        { lat: 4.6392, lng: -74.1003, name: "Biblioteca Virgilio Barco", address: "Av. Carrera 60 #57-60", category: "public" },
+        { lat: 4.5981, lng: -74.0758, name: "Biblioteca El Tintal", address: "Av. Ciudad de Cali #6C-09", category: "public" },
+        { lat: 4.6533, lng: -74.0571, name: "Biblioteca Gabriel García Márquez", address: "Calle 57 #21-66", category: "public" },
+        { lat: 4.7110, lng: -74.0721, name: "Biblioteca Julio Mario Santo Domingo", address: "Calle 170 #67-51", category: "public" },
+        { lat: 4.6486, lng: -74.1105, name: "Biblioteca El Tunal", address: "Calle 48B Sur #21-13", category: "children" },
+        { lat: 4.6243, lng: -74.0642, name: "Biblioteca Luis Ángel Arango", address: "Calle 11 #4-14", category: "university" },
+        { lat: 4.6097, lng: -74.1284, name: "Biblioteca Perdomo", address: "Carrera 1H #62-33 Sur", category: "children" },
+        { lat: 4.6708, lng: -74.0543, name: "Biblioteca Parque El Salitre", address: "Calle 63 #59A-06", category: "children" },
+        { lat: 4.6389, lng: -74.0837, name: "Biblioteca Restrepo", address: "Carrera 20 #11-25 Sur", category: "public" },
+        { lat: 4.6800, lng: -74.0472, name: "Biblioteca Lago Timiza", address: "Transversal 78K #41A-04 Sur", category: "university" }
+    ];
     
-    const library3 = { lat: 4.6533, lng: -74.0571 };
-    const marker3 = new google.maps.Marker({
-        position: library3,
-        map: map,
-        title: "Biblioteca Gabriel García Márquez"
-    });
-    const infoWindow3 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca Gabriel García Márquez</h5><p>Calle 57 #21-66</p>"
-    });
-    marker3.addListener("click", function() {
-        infoWindow3.open(map, marker3);
-    });
-    const library4 = { lat: 4.7110, lng: -74.0721 };
-    const marker4 = new google.maps.Marker({
-        position: library4,
-        map: map,
-        title: "Biblioteca Julio Mario Santo Domingo"
-    });
-    const infoWindow4 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca Julio Mario Santo Domingo</h5><p>Calle 170 #67-51</p>"
-    });
-    marker4.addListener("click", function() {
-        infoWindow4.open(map, marker4);
-    });
-    
-    const library5 = { lat: 4.6486, lng: -74.1105 };
-    const marker5 = new google.maps.Marker({
-        position: library5,
-        map: map,
-        title: "Biblioteca El Tunal"
-    });
-    const infoWindow5 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca El Tunal</h5><p>Calle 48B Sur #21-13</p>"
-    });
-    marker5.addListener("click", function() {
-        infoWindow5.open(map, marker5);
-    });
-    
-    const library6 = { lat: 4.6243, lng: -74.0642 };
-    const marker6 = new google.maps.Marker({
-        position: library6,
-        map: map,
-        title: "Biblioteca Luis Ángel Arango"
-    });
-    const infoWindow6 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca Luis Ángel Arango</h5><p>Calle 11 #4-14</p>"
-    });
-    marker6.addListener("click", function() {
-        infoWindow6.open(map, marker6);
-    });
-    
-    const library7 = { lat: 4.6097, lng: -74.1284 };
-    const marker7 = new google.maps.Marker({
-        position: library7,
-        map: map,
-        title: "Biblioteca Perdomo"
-    });
-    const infoWindow7 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca Perdomo</h5><p>Carrera 1H #62-33 Sur</p>"
-    });
-    marker7.addListener("click", function() {
-        infoWindow7.open(map, marker7);
-    });
-    
-    const library8 = { lat: 4.6708, lng: -74.0543 };
-    const marker8 = new google.maps.Marker({
-        position: library8,
-        map: map,
-        title: "Biblioteca Parque El Salitre"
-    });
-    const infoWindow8 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca Parque El Salitre</h5><p>Calle 63 #59A-06</p>"
-    });
-    marker8.addListener("click", function() {
-        infoWindow8.open(map, marker8);
-    });
-    
-    const library9 = { lat: 4.6389, lng: -74.0837 };
-    const marker9 = new google.maps.Marker({
-        position: library9,
-        map: map,
-        title: "Biblioteca Restrepo"
-    });
-    const infoWindow9 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca Restrepo</h5><p>Carrera 20 #11-25 Sur</p>"
-    });
-    marker9.addListener("click", function() {
-        infoWindow9.open(map, marker9);
-    });
-    
-    const library10 = { lat: 4.6800, lng: -74.0472 };
-    const marker10 = new google.maps.Marker({
-        position: library10,
-        map: map,
-        title: "Biblioteca Lago Timiza"
-    });
-    const infoWindow10 = new google.maps.InfoWindow({
-        content: "<h5>Biblioteca Lago Timiza</h5><p>Transversal 78K #41A-04 Sur</p>"
-    });
-    marker10.addListener("click", function() {
-        infoWindow10.open(map, marker10);
+    libraries.forEach(function(lib) {
+        const marker = new google.maps.Marker({
+            position: { lat: lib.lat, lng: lib.lng },
+            map: map,
+            title: lib.name
+        });
+        
+        const infoWindow = new google.maps.InfoWindow({
+            content: "<h5>" + lib.name + "</h5><p>" + lib.address + "</p>"
+        });
+        
+        marker.addListener("click", function() {
+            infoWindow.open(map, marker);
+        });
+        
+        marker.category = lib.category;
+        markers.push(marker);
     });
 }
