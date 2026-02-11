1395d7fb320fc745aff819a1669f65cd17330736
Camilo-Becerra <camilo.becerra-figueredo@mohawkcollege.ca>
2026-02-11 15:28:01 -0500

    add location of library and marker to map

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function initMap() {
     const bogota = { lat: 4.6097, lng: -74.0817 };
     
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12,
+        zoom: 11,
         center: bogota
     });
-const marker1 = new google.maps.Marker({
+    const library1 = { lat: 4.6392, lng: -74.1003 };
+    const marker1 = new google.maps.Marker({
         position: library1,
         map: map,
         title: "Biblioteca Virgilio Barco"
-    });}
+    });
+}
